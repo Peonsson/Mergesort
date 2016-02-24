2da8e951f896e42b73fd4d89e886380f802f055e
Peonsson <xsnudelx@hotmail.com>
2016-02-24 18:02:19 +0100

    Initial commit

---
+++ src/test/java/MergesortTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by Peonsson on 24/02/16.
+ */
+public class MergesortTest {
+
+    @Test
+    public void testStartNewArray() throws Exception {
+
+
+    }
+}
