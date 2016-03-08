8f971ee3b419187cecaa9fb50988ad36c227b237
roppe546 <roppe546@gmail.com>
2016-03-08 15:29:20 +0100

    Preliminary fix for testing threshold values for merge sort.

--- src/test/java/MergeSortParallelAlphaTest.java
+++ src/test/java/MergeSortParallelAlphaTest.java
@@ -1,7 +1,10 @@
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 
 /**
  * Created by Peonsson and roppe546 on 25/02/16.
@@ -20,33 +23,59 @@ public class MergeSortParallelAlphaTest {
 
         long avg = 0;
 
-        for (int i = 1; i < 110002; i = i + 1332) {
+        int[] thresholds = {100, 150, 200, 250, 300, 350, 500, 750, 1000, 1500, 2000, 2500, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
 
-            for (int j = 0; j < 21; j++) {
-
+        for (int threshold : thresholds) {
+            for (int i = 0; i < 6; i++) {
                 float[] floatCopy = Arrays.copyOfRange(floats, 0, floats.length);
 
                 System.gc();
                 Thread.sleep(150);
 
-                long test = System.currentTimeMillis();
-                new MergeSortParallelAlpha(floatCopy, i).compute();
-                long data = System.currentTimeMillis() - test;
+                ForkJoinPool pool = new ForkJoinPool();
+                RecursiveAction task = new MergeSortParallelAlpha(floatCopy, threshold);
+                long starTime = System.currentTimeMillis();
+                pool.invoke(task);
+                long data = System.currentTimeMillis() - starTime;
 
-                if (j > 0)
+                if (threshold > 0)
                     avg += data;
             }
 
-            System.out.println("thresh: " + i + ", avg: " + avg / 20);
+            System.out.println("thresh: " + threshold + ", avg: " + avg / 5);
             avg = 0;
-
         }
 
-//        boolean isSorted = true;
+//        for (int i = 100; i <= 10000; i = i + 50) {
 //
-//        for (int i = 1; i < floats.length - 1; i++) {
-//            if (floats[i - 1] > floats[i])
-//                isSorted = false;
+//            for (int j = 0; j < 21; j++) {
+//
+//                float[] floatCopy = Arrays.copyOfRange(floats, 0, floats.length);
+//
+//                System.gc();
+//                Thread.sleep(150);
+//
+//                long test = System.currentTimeMillis();
+//                new MergeSortParallelAlpha(floatCopy, i).compute();
+//                long data = System.currentTimeMillis() - test;
+//
+//                if (j > 0)
+//                    avg += data;
+//            }
+//
+//            System.out.println("thresh: " + i + ", avg: " + avg / 20);
+//            avg = 0;
+//
 //        }
+    }
+
+
+    private boolean isSorted(float[] array) {
+        for (int i = 1; i < array.length - 1; i++) {
+            if (array[i - 1] > array[i])
+                return false;
+        }
+
+        return true;
     }
 }
